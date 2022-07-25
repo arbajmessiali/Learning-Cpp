7b6c73b485ba6485f92855aa80bcb1700d8b0a7f
Intaj Ali <intajcse@gmail.com>
2022-07-25 15:08:58 +0530

    using mathematical operations

---
+++ math.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+int a = 10;
+int b = 7;
+std::cout<<"The sum of 10 and 7 is "<<(a+b)<<std::endl;
+std::cout<<"The difference of 10 and 7 is "<<(a-b)<<std::endl;
+std::cout<<"The product of 10 and 7 is "<<(a*b)<<std::endl;
+std::cout<<"The division of 10 and 7 is "<<(double)a/(double)b<<std::endl;
+std::cout<<"The modulus of 10 and 7 is "<<(a%b)<<std::endl;
+return 0;
+}
