cf52dc5d4c2f364c3739a8da2c8783e62a860a91
Intaj Ali <intajcse@gmail.com>
2022-07-28 15:29:55 +0530

    create and call function

---
+++ func.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//creating a function
+void call(){
+	cout<<"I have been called"<<endl;
+}
+	
+int main(){
+	//calling the function
+	call();
+	call();
+	
+	return 0;
+}
